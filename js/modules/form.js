acc568d7e2f46570beca68dc2502be57be801219
Nurken Zhappargaly <zhnurken@gmail.com>
2023-02-14 18:00:33 +0600

    first commit

---
+++ js/modules/form.js
@@ -0,0 +1,79 @@
+import { openModal, closeModal } from "./modal";
+import { postData } from "../services/services";
+
+function form(formSelector, modalTimerId){
+    
+    const forms = document.querySelectorAll(formSelector);
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        bindpostData(item);
+    });
+
+
+
+
+    function bindpostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+        
+
+
+            const formData = new FormData(form);
+
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+
+            postData('http://localhost:3000/requests', json)
+            .then(data=>{
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(()=>{
+                showThanksModal(message.failure);
+            }).finally(()=>{
+                form.reset(); 
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal('.modal', modalTimerId);
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal('.modal');
+        }, 4000);
+    }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+}
+export default form;
---
+++ js/script.js
@@ -0,0 +1,31 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calculate from './modules/calculate';
+import form from './modules/form';
+import slider from './modules/slider';
+import { openModal } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+          
+const modalTimerId = setTimeout(()=> openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2023-02-25');
+    cards();
+    calculate();
+    form('form', modalTimerId);
+    slider({
+        container:'.offer__slider',
+        slide:'.offer__slide',
+        nextArrow:'.offer__slider-next',
+        prevArrow:'.offer__slider-prev',
+        totalCounter:'#total',
+        currentCounter:'#current',
+        wrapper:'.offer__slider-wrapper',
+        field:'.offer__slider-inner'
+    });           
+
+});
